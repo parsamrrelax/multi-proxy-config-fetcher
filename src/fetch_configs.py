a369dab3283bd3353ea01c6c747df0e534ff1ac7
Parsa <40731455+parsamrrelax@users.noreply.github.com>
2025-01-12 13:12:46 +0330

    Update fetch_configs.py

--- src/fetch_configs.py
+++ src/fetch_configs.py
@@ -219,7 +219,7 @@ def save_configs(configs: List[str], config: ProxyConfig):
     try:
         os.makedirs(os.path.dirname(config.OUTPUT_FILE), exist_ok=True)
         with open(config.OUTPUT_FILE, 'w', encoding='utf-8') as f:
-            header = """//profile-title: base64:8J+NhldhcnDwn42G
+            header = """//profile-title: base64:8J+Nhm5vdF9zYWZl8J+Nhg==
 //profile-update-interval: 1
 //subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531
 //profile-web-page-url: https://github.com/parsamrrelax
